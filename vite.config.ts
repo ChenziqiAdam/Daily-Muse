75cf8d206fdb83f25fdcfa6fa4b34d37f615ab23
Adam Chen <adamchen04718@gmail.com>
2025-12-18 20:18:37 +0800

    Update app url

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     return {
+      base: '/Daily-Muse/',
       server: {
         port: 3000,
         host: '0.0.0.0',
